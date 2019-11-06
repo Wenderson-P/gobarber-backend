3196699d1efc6d9516dec5556e9a9310be9aba31
Wenderson <wps713@gmail.com>
2019-11-06 14:53:22 -0300

    Adding multer in routes
    Now we can send images through baseurl/files

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,14 @@
 import { Router } from 'express';
 
+import multer from 'multer';
+import multerConfig from './config/multer';
+
 import SessionController from './app/controllers/SessionController';
 import UserController from './app/controllers/UserController';
 import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
+const upload = multer(multerConfig);
 
 routes.post('/users', UserController.store);
 routes.post('/sessions', SessionController.store);
@@ -13,4 +17,5 @@ routes.post('/sessions', SessionController.store);
 routes.use(authMiddleware);
 routes.put('/users', UserController.update);
 
+routes.post('/files', upload.single('file'), (req, res) => res.json({ ok: true }));
 export default routes;
