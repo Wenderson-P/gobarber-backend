256964a6874754e673dfc16ef6c0bb5392d8f96c
Wenderson <wps713@gmail.com>
2019-11-08 19:09:07 -0300

    Configurating mailtrap
    THESE CREDENTIALS WILL ONLY WORK IN MY MAILTRAP EMAIL, PLS CREATE A ACCOUNT IN https://mailtrap.io/ and change the credentials

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: '7482d4bb3ef726',
+    pass: '1a550d38606944',
+  },
+  default: {
+    from: 'Equipe Go Barber <noreply@gobarber.com>',
+  },
+};
