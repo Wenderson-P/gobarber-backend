7c9ec214005f44f7cd9e89e497e037452ea10b27
Wenderson <wps713@gmail.com>
2019-11-08 15:45:05 -0300

    Checking if user is a provider

--- src/app/controllers/ScheduleController.js
+++ src/app/controllers/ScheduleController.js
@@ -1,7 +1,15 @@
+import User from '../models/User';
 import Appointment from '../models/Appointment';
 
 class ScheduleController {
   async index(req, res) {
+    const checkUserProvider = await User.findOne({
+      where: { id: req.userId, provider: true },
+    });
+    if (!checkUserProvider) {
+      return res.status(401).json('User is not a provider');
+    }
+
     return res.json('ok');
   }
 }
