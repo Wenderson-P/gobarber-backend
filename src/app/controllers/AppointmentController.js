4755d2cf2a7ad4b321a1802dc006f279492293ec
Wenderson <wps713@gmail.com>
2019-11-08 19:01:31 -0300

    Adding code to cancel the appointment

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -1,5 +1,5 @@
 import * as Yup from 'yup';
-import { startOfHour, parseISO, isBefore, format } from 'date-fns';
+import { startOfHour, parseISO, isBefore, format, subHours } from 'date-fns';
 import pt from 'date-fns/locale/pt';
 import User from '../models/User';
 import Appointment from '../models/Appointment';
@@ -116,6 +116,19 @@ class AppointmentController {
         error: "You don't have permission to cancel this appointment.",
       });
     }
+
+    // Removing hours from appointment
+    const dateWithSub = subHours(appointment.date, 2);
+
+    if (isBefore(dateWithSub, new Date())) {
+      return res.status(401).json({
+        error: 'You can only cancel appointments 2 hours in advance.',
+      });
+    }
+
+    appointment.canceled_at = new Date();
+
+    await appointment.save();
 
     return res.json();
   }
