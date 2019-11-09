a62036d7bf07fd385601af628c4cb89215e78f6d
Wenderson <wps713@gmail.com>
2019-11-09 15:47:26 -0300

    Searching for todays appointments in available controller

--- src/app/controllers/AvaliableController.js
+++ src/app/controllers/AvaliableController.js
@@ -1,6 +1,25 @@
+import { startOfDay, endOfDay } from 'date-fns';
+import { Op } from 'sequelize';
+import Appointment from '../models/Appointment';
+
 class AvaliableController {
   async index(req, res) {
-    return res.json('ok');
+    const { date } = req.query;
+    if (!date) {
+      return res.status(400).json({ error: 'Invalid date' });
+    }
+    const searchDate = Number(date);
+    const appointments = await Appointment.findAll({
+      where: {
+        provider_id: req.params.providerId,
+        canceled_at: null,
+        date: {
+          [Op.between]: [startOfDay(searchDate), endOfDay(searchDate)],
+        },
+      },
+    });
+
+    return res.json(appointments);
   }
 }
 
