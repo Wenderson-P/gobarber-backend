d0db743029a902850230c46a8a22e620ca3ba155
Wenderson <wps713@gmail.com>
2019-11-09 19:18:54 -0300

    Changing static variables to process.env
    We need to import dotenv/config in app.js, queue.js and database.js. 
    Also, in the exception handler, we now check if the environment is equal to development.

--- src/config/mail.js
+++ src/config/mail.js
@@ -1,10 +1,10 @@
 export default {
-  host: 'smtp.mailtrap.io',
-  port: 2525,
+  host: process.env.MAIL_HOST,
+  port: process.env.MAIL_PORT,
   secure: false,
   auth: {
-    user: '7482d4bb3ef726',
-    pass: '1a550d38606944',
+    user: process.env.MAIL_USER,
+    pass: process.env.MAIL_PASS,
   },
   default: {
     from: 'Equipe Go Barber <noreply@gobarber.com>',
