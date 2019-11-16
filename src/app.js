46d355690cff97e4d82f90e103e051c87aff55e2
Wenderson <wps713@gmail.com>
2019-11-15 23:06:16 -0300

    Setting cors on server

--- src/app.js
+++ src/app.js
@@ -3,10 +3,10 @@ import 'dotenv/config';
 import express from 'express';
 import path from 'path';
 import Youch from 'youch';
+import cors from 'cors';
 import * as Sentry from '@sentry/node';
 import sentryConfig from './config/sentry';
 import 'express-async-errors';
-
 import routes from './routes';
 
 import './database';
@@ -22,6 +22,7 @@ class App {
 
   middlewares() {
     this.server.use(Sentry.Handlers.requestHandler());
+    this.server.use(cors());
     this.server.use(express.json());
     this.server.use(
       '/files',
