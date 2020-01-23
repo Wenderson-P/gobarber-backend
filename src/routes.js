82037c2250ae827a1d350d2cad15c487cdc542cd
Wenderson <wps713@gmail.com>
2020-01-23 18:54:12 -0300

    Add validators in routes and using as middlewares

--- src/routes.js
+++ src/routes.js
@@ -12,24 +12,37 @@ import ScheduleController from './app/controllers/ScheduleController';
 import NotificationController from './app/controllers/NotificationController';
 import AvailableController from './app/controllers/AvailableController';
 
+import validateUserStore from './app/validators/UserStore';
+import validateUserUpdate from './app/validators/UserUpdate';
+
+import validateSessionStore from './app/validators/sessionStore';
+
+import validateAppointmentStore from './app/validators/appointmentStore';
+
 import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
 const upload = multer(multerConfig);
 
-routes.post('/users', UserController.store);
-routes.post('/sessions', SessionController.store);
+routes.post('/users', validateUserStore, UserController.store);
+routes.post('/sessions', validateSessionStore, SessionController.store);
 routes.get('/', (req, res) => res.send('Tudo ok'));
 
 // Como está depois das 2 rotas, o que estiver encima não vai usar o middleware
 routes.use(authMiddleware);
-routes.put('/users', UserController.update);
+routes.put('/users', validateUserUpdate, UserController.update);
 
 routes.get('/providers', ProviderController.index);
 routes.get('/providers/:providerId/available', AvailableController.index);
 
 routes.get('/appointments', AppointmentController.index);
-routes.post('/appointments', AppointmentController.store);
+
+routes.post(
+  '/appointments',
+  validateAppointmentStore,
+  AppointmentController.store
+);
+
 routes.delete('/appointments/:id', AppointmentController.delete);
 
 routes.get('/schedule', ScheduleController.index);
