06d06750c4d575342175b80463ffa8a720aee763
Wenderson <wps713@gmail.com>
2020-01-24 19:04:35 -0300

    Invalidating cache when appointment is created

--- src/app/services/CreateAppointmentService.js
+++ src/app/services/CreateAppointmentService.js
@@ -5,6 +5,7 @@ import pt from 'date-fns/locale/pt';
 import User from '../models/User';
 import Appointment from '../models/Appointment';
 import Notification from '../schemas/Notification';
+import Cache from '../../lib/Cache';
 
 class CreateAppointmentService {
   async run({ provider_id, user_id, date }) {
@@ -58,6 +59,8 @@ class CreateAppointmentService {
       content: `Novo agendamento de ${user.name}para o ${formattedDate}`,
       user: provider_id,
     });
+
+    await Cache.invalidatePrefix(`user:${user_id}:appointments`);
 
     return appointment;
   }
